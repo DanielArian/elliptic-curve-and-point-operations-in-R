6dc194e253998c45f8da9865316b1676ee357603
DanielArian <71820558+DanielArian@users.noreply.github.com>
2022-03-28 20:10:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// file to export classes, functions or variables to other modules
+// export classes, functions or variables to other modules
 
-import { Point } from './Point.js'
-import { Weierstrass, WeierstrassPoint } from './supported_curves/Weierstrass.js'
-
-export { Point }
-export { Weierstrass, WeierstrassPoint }
+export { Point } from './Point.js'
+export { Weierstrass, WeierstrassPoint } from './supported_curves/Weierstrass.js'
